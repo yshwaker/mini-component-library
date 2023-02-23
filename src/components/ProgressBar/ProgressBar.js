ee0a660a7cd64195cc8457339b317a3437f6dcdb
yshwaker <yshwaker@gmail.com>
2023-02-23 12:38:25 +0800

    set height at the child level; create concentric circles; use overflow to trim corner

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -7,36 +7,43 @@ import VisuallyHidden from '../VisuallyHidden';
 
 const SIZES = {
   small: {
-    '--height': 8 + 'px',
-    '--border-radius': 4 + 'px',
-    '--padding': 0 + 'px',
+    height: 8,
+    radius: 4,
+    padding: 0,
   },
   medium: {
-    '--height': 12 + 'px',
-    '--border-radius': 4 + 'px',
-    '--padding': 0 + 'px',
+    height: 12,
+    radius: 4,
+    padding: 0,
   },
   large: {
-    '--height': 24 + 'px',
-    '--border-radius': 8 + 'px',
-    '--padding': 4 + 'px',
+    height: 16,
+    radius: 8,
+    padding: 4,
   },
 }
 
 const ProgressBar = ({ value, size }) => {
-  const style = SIZES[size]
-  const borderRadius = value === 100 ? '4px' : '4px 0px 0px 4px'
+  const styles = SIZES[size]
   return (
-    <Wrapper style={style}>
-      <VisuallyHidden>
-        <span id="loadinglabel">Loading:</span>
-      </VisuallyHidden>
-      <Progress
-        role="progressbar"
-        aria-labelledby="loadinglabel"
-        aria-valuenow={value}
-        style={{ '--width': value + '%', '--border-radius': borderRadius }}
-      />
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        '--padding': styles.padding + 'px',
+        '--radius': styles.radius + 'px',
+      }}
+    >
+      <BarWrapper>
+        <Bar
+          // we apply the height in the child instead of the parent. since we usually look down the tree to decide the height itself.
+          style={{ '--width': value + '%', '--height': styles.height + 'px' }}
+        >
+          <VisuallyHidden>{value}%</VisuallyHidden>
+        </Bar>
+      </BarWrapper>
     </Wrapper>
   )
 }
@@ -44,16 +51,23 @@ const ProgressBar = ({ value, size }) => {
 const Wrapper = styled.div`
   height: var(--height);
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  border-radius: var(--border-radius);
+  border-radius: var(--radius);
   background-color: ${COLORS.transparentGray15};
   padding: var(--padding);
 `
 
-const Progress = styled.div`
-  width: var(--width);
+const BarWrapper = styled.div`
+  /* trim off corner when progress bar is near full */
+  overflow: hidden;
+  border-radius: 4px;
   height: 100%;
+`
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
   background-color: ${COLORS.primary};
-  border-radius: var(--border-radius);
+  border-radius: 4px 0 0 4px;
 `
 
 
