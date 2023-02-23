ea8703fca1d9bc75f18e3c18aee1f5a18679aa15
yshwaker <yshwaker@gmail.com>
2023-02-23 17:32:25 +0800

    simplify

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -10,22 +10,27 @@ const STYLES = {
   small: {
     fontSize: 14 / 16,
     borderWidth: 1,
-    padding: 3,
-    paddingLeft: 22,
-    iconSize: 18,
+    height: 24 / 16,
+    iconSize: 16,
     strokeWidth: 1,
   },
   large: {
     fontSize: 18 / 16,
     borderWidth: 2,
-    padding: 5,
-    paddingLeft: 36,
+    height: 36 / 16,
     iconSize: 24,
     strokeWidth: 2,
   },
 }
 
-const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+const IconInput = ({
+  label,
+  icon,
+  width = 250,
+  size,
+  placeholder,
+  ...delegated
+}) => {
   const styles = STYLES[size]
 
   if (!styles) {
@@ -34,7 +39,7 @@ const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
 
   return (
     <Wrapper>
-      <VisuallyHidden>{icon}</VisuallyHidden>
+      <VisuallyHidden>{label}</VisuallyHidden>
       <InputIcon
         size={styles.iconSize}
         strokeWidth={styles.strokeWidth}
@@ -44,19 +49,20 @@ const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
         placeholder={placeholder}
         style={{
           '--width': width + 'px',
+          '--height': styles.height + 'rem',
           '--font-size': styles.fontSize + 'rem',
           '--border-width': styles.borderWidth + 'px',
-          '--padding-top': styles.padding + 2 + 'px',
-          '--padding-bottom': styles.padding + 'px',
-          '--padding-left': styles.paddingLeft + 'px',
         }}
         value={label}
+        {...delegated}
       />
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div`
+// when you click anything inside label, it auto select the input.
+const Wrapper = styled.label`
+  display: block;
   position: relative;
   color: ${COLORS.gray700};
   width: max-content;
@@ -72,20 +78,19 @@ const InputIcon = styled(Icon)`
   top: 0;
   bottom: 0;
   margin: auto;
-  pointer-events: none;
 `
 
 const Input = styled.input`
   border: 0;
   color: inherit;
   width: var(--width);
+  height: var(--height);
+
   font-size: var(--font-size);
   font-family: 'Roboto', sans-serif;
   font-weight: 700;
   border-bottom: var(--border-width) solid ${COLORS.black};
-  padding-top: var(--padding-top);
-  padding-bottom: var(--padding-bottom);
-  padding-left: var(--padding-left);
+  padding-left: var(--height);
 
   &::placeholder {
     font-weight: 400;
